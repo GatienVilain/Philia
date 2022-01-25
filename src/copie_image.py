775b9d874bc9b78b3217686eaa936d30ec83a608
GatienVilain <Gatien.vilain@outlook.fr>
2022-01-25 23:21:22 +0100

    Ajout de la fonction de copie des images

---
+++ src/copie_image.py
@@ -0,0 +1,9 @@
+import os
+
+
+def copie_images(chemin_entree, chemin_destination, liste_element):
+    for element in liste_element:
+        if element.image:
+            os.system("powershell copy " + chemin_entree + "/" + element.contenu +
+                      " " + chemin_destination + "./site/images -erroraction 'silentlycontinue'")
+    return
