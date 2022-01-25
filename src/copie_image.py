8b4040e3b89357da559e305b69edbaf99e59f49c
GatienVilain <Gatien.vilain@outlook.fr>
2022-01-25 23:24:09 +0100

    correction d'une erreur de la commande système

--- src/copie_image.py
+++ src/copie_image.py
@@ -5,5 +5,5 @@ def copie_images(chemin_entree, chemin_destination, liste_element):
     for element in liste_element:
         if element.image:
             os.system("powershell copy " + chemin_entree + "/" + element.contenu +
-                      " " + chemin_destination + "./site/images -erroraction 'silentlycontinue'")
+                      " " + chemin_destination + "/site/images -erroraction 'silentlycontinue'")
     return
