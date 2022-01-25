a227dbb09804812ecd3518a3269c303d1c0575e2
GatienVilain <Gatien.vilain@outlook.fr>
2022-01-25 23:22:09 +0100

    Ajout de la fonction de génération de toute les pages avec copie des images

--- src/generer_pages.py
+++ src/generer_pages.py
@@ -1,65 +1,76 @@
 import os
 from extraction_mind_map import *
-
-
-def generer_section_1_colonne(contenu_html, titre, element):
-    contenu_html += "<section id=\"" + titre + ">\n\t<h2>" + titre + "</h2>\n\t"
-    if element.image:
-        contenu_html += "<img src=\"" + element.texte + "\">"
-    else:
-        contenu_html += "<p>" + element.texte + "</p>"
-    contenu_html += "\n</section>"
-    return contenu_html
-
-
-def generer_section_2_colonne(contenu_html, titre, element_1, element_2):
-    contenu_html += "<section id=\"" + titre + ">\n\t<h2>" + titre + "</h2>\n\t"
-    if element_1.image:
-        contenu_html += "<img src=\"" + element.texte + "\">"
-    else:
-        contenu_html += "<p>" + element.texte + "</p>"
-    if element_2.image:
-        contenu_html += "<img src=\"" + element.texte + "\">"
-    else:
-        contenu_html += "<p>" + element.texte + "</p>"
-    contenu_html += "</section>"
-    return contenu_html
+from copie_image import *
 
 
 def generer_page(chemin_destination, liste_element):
-    if not os.path.exists(path):
-        os.mkdir(chemin_destination + "site")
+    contenu_html = "<!DOCTYPE html><html lang=\"fr\">\n<head>\n<meta charset=\"UTF-8\">\n<meta http-equiv=\"X-UA-Compatible\" content=\"IE=edge\">\n<meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\">\n\t<title>" + \
+        liste_element[0].contenu + "</title>\n</head>\n<body>\n<h1>" + \
+        liste_element[0].contenu + "</h1>\n"
 
-    contenu_html = "<!DOCTYPE html><html lang=\"fr\">\n<head>\n<meta charset=\"UTF-8\">\n<meta http-equiv=\"X-UA-Compatible\" content=\"IE=edge\">\n<meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\">\n\t<title>" + liste_element[0].texte + "</title>\n</head>\n<body>"
-
-    # boucle d'élément qui génère les pages
-    # ------
-    nombre_element = 0
-    for element in liste_element:
-        if liste_element.title:
-            if nombre_element == 1:
-                generer_section_1_colonne(contenu_html, titre, element_1)
-            elif nombre_element == 2:
-                generer_section_2_colonne(
-                    contenu_html, titre, element_1, element_2)
-
-            titre = liste_element.texte
-            nombre_element = 0
-        elif nombre_element == 0:
-            element_1 = liste_element.texte
-            nombre_element += 1
-        else:
-            element_2 = liste_element.texte
-            nombre_element += 1
-
-    # ------
+    section_ouvert = False
+    if len(liste_element) > 1:
+        for index in range(1, len(liste_element)):
+            element = liste_element[index]
+            if element.sous_titre:
+                if section_ouvert:
+                    contenu_html += "\n</section>"
+                contenu_html += "<section id=\"" + element.contenu + \
+                    "\">\n\t<h2>" + element.contenu + "</h2>\n\t"
+                section_ouvert = True
+            elif element.image:
+                contenu_html += "<img src=\"" + element.contenu + "\">"
+            else:
+                contenu_html += "<p>" + element.contenu + "</p>"
+        if section_ouvert:
+            contenu_html += "\n</section>"
 
     contenu_html += "\n</body>\n</html>"
-    f = open(chemin_destination + "/" + liste_element[0].texte + 'index.html', 'w')
+    f = open(chemin_destination + "/site/" +
+             liste_element[0].contenu.replace(" ", "_") + '.html', 'w', encoding='utf8')
     f.write(contenu_html)
     f.close()
+
     return True
 
 
-l = extraction_texte('./src/TITRE.mm')
-generer_page("./", l)
+def generer_page_accueil(chemin_destination, liste_element):
+    contenu_html = "<!DOCTYPE html><html lang=\"fr\">\n<head>\n<meta charset=\"UTF-8\">\n<meta http-equiv=\"X-UA-Compatible\" content=\"IE=edge\">\n<meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\">\n\t<title>" + \
+        liste_element[0][0].contenu + "</title>\n</head>\n<body>\n<h1>" + \
+        liste_element[0][0].contenu + "</h1>\n<ul>\n"
+
+    for index in range(2, len(liste_element)):
+        contenu_html += "\t<li><a href=" + liste_element[index][0].contenu.replace(
+            " ", "_") + ".html\">" + liste_element[index][0].contenu + "</a></li>\n"
+
+    contenu_html += "\n</body>\n</html>"
+    f = open(chemin_destination + "/site/index.html", 'w', encoding='utf8')
+    f.write(contenu_html)
+    f.close()
+
+    return True
+
+
+def generer_pages(chemin_entree, chemin_destination):
+    fichier_entree = extraction_texte(chemin_entree)
+
+    if not os.path.exists(chemin_destination + "site"):
+        os.mkdir(chemin_destination + "site")
+        os.mkdir(chemin_destination + "site/css")
+        os.mkdir(chemin_destination + "site/images")
+
+    copie_images(chemin_entree, chemin_destination, fichier_entree)
+
+    fichier_entree = decoupage(fichier_entree)
+
+    generer_page_accueil(chemin_destination, fichier_entree)
+    for page in range(1, len(fichier_entree)):
+        generer_page(chemin_destination, fichier_entree[page])
+
+    return True
+
+
+# ---- TEST -------
+if __name__ == '__main__':
+    generer_pages(
+        r"C:\Users\gatie\source\repos\GatienVilain\projet-info\TEST.mm", "./")
